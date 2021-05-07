64cb5a1d419f2b2c82b5f991c2f6e412e3d1568c
Arni <@arnirahayusuryani@gmail.com>
2021-05-07 21:27:28 +0700

    heuheh

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/PraktiKumweb4-MVC/';
+$config['base_url'] = 'http://localhost/PraktiKumweb-MVC/';
 
 /*
 |--------------------------------------------------------------------------
